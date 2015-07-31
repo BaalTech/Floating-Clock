3403fa544d38397092ced274f05f70c72993b235
Baal Evan <baalevan@tlen.pl>
2015-07-31 15:21:19 +0200

    Init

---
+++ FloatingClock/HotKey.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace FloatingClock
+{
+    public class HotKey : IDisposable
+    {
+        private const int WmHotKey = 0x0312;
+        private static Dictionary<int, HotKey> DictHotKeyToCalBackProc;
+        private bool _disposed;
+        // ******************************************************************
+        public HotKey(Key k, KeyModifier keyModifiers, Action<HotKey> action, bool register = true)
+        {
+            Key = k;
+            KeyModifiers = keyModifiers;
+            Action = action;
+            if (register)
+            {
+                Register();
+            }
+        }
+
+        private Key Key { get; }
+        private KeyModifier KeyModifiers { get; }
+        private Action<HotKey> Action { get; }
+        private int Id { get; set; }
+        // ******************************************************************
+        // Implement IDisposable.
+        // Do not make this method virtual.
+        // A derived class should not be able to override this method.
+        public void Dispose()
+        {
+            Dispose(true);
+            // This object will be cleaned up by the Dispose method.
+            // Therefore, you should call GC.SupressFinalize to
+            // take this object off the finalization queue
+            // and prevent finalization code for this object
+            // from executing a second time.
+            GC.SuppressFinalize(this);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vlc);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        // ******************************************************************
+        private bool Register()
+        {
+            var virtualKeyCode = KeyInterop.VirtualKeyFromKey(Key);
+            Id = virtualKeyCode + ((int) KeyModifiers*0x10000);
+            var result = RegisterHotKey(IntPtr.Zero, Id, (uint) KeyModifiers, (uint) virtualKeyCode);
+
+            if (DictHotKeyToCalBackProc == null)
+            {
+                DictHotKeyToCalBackProc = new Dictionary<int, HotKey>();
+                ComponentDispatcher.ThreadFilterMessage += ComponentDispatcherThreadFilterMessage;
+            }
+
+            DictHotKeyToCalBackProc.Add(Id, this);
+
+            // Debug.Print(result + ", " + Id + ", " + virtualKeyCode);
+            return result;
+        }
+
+        // ******************************************************************
+        private void Unregister()
+        {
+            HotKey hotKey;
+            if (DictHotKeyToCalBackProc.TryGetValue(Id, out hotKey))
+            {
+                UnregisterHotKey(IntPtr.Zero, Id);
+            }
+        }
+
+        // ******************************************************************
+        private static void ComponentDispatcherThreadFilterMessage(ref MSG msg, ref bool handled)
+        {
+            if (handled) return;
+            if (msg.message != WmHotKey) return;
+            HotKey hotKey;
+
+            if (!DictHotKeyToCalBackProc.TryGetValue((int) msg.wParam, out hotKey)) return;
+            hotKey.Action?.Invoke(hotKey);
+            handled = true;
+        }
+
+        // ******************************************************************
+        // Dispose(bool disposing) executes in two distinct scenarios.
+        // If disposing equals true, the method has been called directly
+        // or indirectly by a user's code. Managed and unmanaged resources
+        // can be _disposed.
+        // If disposing equals false, the method has been called by the
+        // runtime from inside the finalizer and you should not reference
+        // other objects. Only unmanaged resources can be _disposed.
+        private void Dispose(bool disposing)
+        {
+            // Check to see if Dispose has already been called.
+            if (_disposed) return;
+            // If disposing equals true, dispose all managed
+            // and unmanaged resources.
+            if (disposing)
+            {
+                // Dispose managed resources.
+                Unregister();
+            }
+
+            // Note disposing has been done.
+            _disposed = true;
+        }
+    }
+
+    // ******************************************************************
+    [Flags]
+    public enum KeyModifier
+    {
+        None = 0x0000,
+        Alt = 0x0001,
+        Ctrl = 0x0002,
+        NoRepeat = 0x4000,
+        Shift = 0x0004,
+        Win = 0x0008
+    }
+
+    // ******************************************************************
+}
---
+++ FloatingClock/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Application = System.Windows.Application;
+using MouseEventArgs = System.Windows.Forms.MouseEventArgs;
+
+namespace FloatingClock
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+
+        private System.Windows.Forms.NotifyIcon notifyIcon = null;
+
+        private DispatcherTimer refreshDispatcher;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Refresh("", new EventArgs());
+            ShowClock();
+            InitializeDispatcher();
+            WaitToFullMinuteAndRefresh();
+            new HotKey(Key.C, KeyModifier.Alt, key => ShowClock());
+            TrayIcon();
+        }
+
+        private void TrayIcon()
+        {
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Click += new EventHandler(notifyIcon_Click);
+            notifyIcon.Icon = new Icon(Application.GetResourceStream(new Uri("pack://application:,,,/clock.ico")).Stream);
+            notifyIcon.Visible = true;
+            notifyIcon.ShowBalloonTip(5, "Hello " + Environment.UserName, "Press Alt+C to show Clock\nRight Click on Tray to Close", ToolTipIcon.Info);
+        }
+        void notifyIcon_Click(object sender, EventArgs e)
+        {
+            if ((e as MouseEventArgs).Button == MouseButtons.Right)
+                Close();
+        }
+        //private void InitializeBattery()
+        //{
+
+        //   if (SystemInformation.PowerStatus.BatteryChargeStatus != BatteryChargeStatus.NoSystemBattery)
+        //{
+        //    string path = "/Assets/";
+        //    if (SystemInformation.PowerStatus.BatteryChargeStatus == BatteryChargeStatus.Charging)
+        //    {
+        //        path += "Charging/";
+        //        path += (SystemInformation.PowerStatus.BatteryLifePercent * 100) / 10 + ".png";
+
+        //    }
+        //    else
+        //    {
+        //        path += "Normal/";
+        //        path += (SystemInformation.PowerStatus.BatteryLifePercent * 100) / 10 + ".png";
+
+        //    }
+        //    BatteryImage.Source = new BitmapImage(new Uri("pack://application:,,,/FloatingClock;component" + path, UriKind.Absolute));
+
+        //    Battery.Text = (SystemInformation.PowerStatus.BatteryLifePercent * 100).ToString();
+        //}
+
+
+        //}
+
+        private void ShowClock()
+        {
+            SetPosition();
+            Refresh("", new EventArgs());
+            InitializeAnimationIn();
+
+            WaitToFullMinuteAndRefresh();
+        }
+
+        private void InitializeAnimationIn()
+        {
+            Application.Current.MainWindow.Activate();
+            var dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += OpacityFadeIn;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 5);
+            Application.Current.MainWindow.Visibility = Visibility.Visible;
+
+            dispatcherTimer.Start();
+        }
+
+        private void SetPosition()
+        {
+            //var mousePosition = Control.MousePosition;
+            var activeScreen = Screen.FromPoint(Control.MousePosition);
+            //var screenHeight = activeScreen.Bounds.Height + activeScreen.Bounds.Y;
+            Application.Current.MainWindow.Top = (activeScreen.Bounds.Height + activeScreen.Bounds.Y) - 140 - 48;
+            Application.Current.MainWindow.Left = activeScreen.Bounds.X + 50;
+        }
+
+        private void InitializeClock()
+        {
+            var timeNow = DateTime.Now;
+            Hours.Text = timeNow.ToString("HH");
+            Minutes.Text = timeNow.ToString("mm");
+            DayOfTheWeek.Text = timeNow.ToString("dddd");
+            DayOfTheMonth.Text = timeNow.ToString("dd") + " " + timeNow.ToString("MMMM");
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            var dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += OpacityFadeOut;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 15);
+
+            dispatcherTimer.Start();
+            refreshDispatcher.Stop();
+        }
+
+        private static void OpacityFadeOut(object sender, EventArgs e)
+        {
+            if (Application.Current.MainWindow.Opacity > 0)
+                Application.Current.MainWindow.Opacity -= 0.1;
+            else
+            {
+                ((DispatcherTimer)sender).Stop();
+                Application.Current.MainWindow.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private static void OpacityFadeIn(object sender, EventArgs e)
+        {
+            if (Application.Current.MainWindow.Opacity < 0.95)
+                Application.Current.MainWindow.Opacity += 0.05;
+            else
+                ((DispatcherTimer)sender).Stop();
+        }
+
+        private async void WaitToFullMinuteAndRefresh()
+        {
+            await Task.Delay((60 - DateTime.Now.Second) * 1000);
+            Refresh("", new EventArgs());
+            refreshDispatcher.Start();
+        }
+
+        private void InitializeDispatcher()
+        {
+            refreshDispatcher = new DispatcherTimer();
+            refreshDispatcher.Tick += Refresh;
+            refreshDispatcher.Interval = new TimeSpan(0, 1, 0);
+        }
+
+        private void Refresh(object sender, EventArgs e)
+        {
+            InitializeClock();
+            //   InitializeBattery();
+        }
+    }
+}
